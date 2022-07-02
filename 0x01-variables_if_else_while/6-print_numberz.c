7b85d0d54d0994e5e532787b3d78684d84b8371b
Ayandare Samuel <samuelayandare@gmail.com>
2022-07-01 20:50:49 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,5 @@
  #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0
@@ -6,8 +7,8 @@
 int main(void)
 {
 	int num;
-	for(num=0; num<10; num++)
-		putchar((num%10)+'0');
+	for(num = 0; num < 10; num++)
+		putchar((num % 10 ) + '0' );
 	putchar('\n');
 	return (0);
 }
