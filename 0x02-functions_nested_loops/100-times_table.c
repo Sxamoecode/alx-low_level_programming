ee048d76ec7e38266a7b75a4528b8f6e4f9721d2
Ayandare Samuel <samuelayandare@gmail.com>
2022-07-05 13:55:01 -0700

    0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,15 +22,15 @@ void print_times_table(int n)
 	p = m * a;
 	if (c == 0 && p < 10)
 	{
-	printf(",  %d", p);
+	printf(",   %d", p);
 	}
 	else if (c == 0 && p < 100)
 	{
-	printf(",  %d", p);
+	printf(",   %d", p);
 	}
 	else if (c == 0 && p < 1000)
 	{
-	printf(",  %d", p);
+	printf(",   %d", p);
 	}
 	else if (c == 0 && p >= 1000)
 	{
