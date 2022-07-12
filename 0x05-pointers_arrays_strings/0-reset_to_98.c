cc9af426e9ff2e8cac55f78391d08f1921f7d51c
Ayandare Samuel <samuelayandare@gmail.com>
2022-07-12 09:26:20 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -13,6 +13,6 @@ void reset_to_98(int *n);
     printf("n=%d\n", n);
     ptr_n = &n;
     *ptr_n = 'b';
-        printf("n=%d\n", n);
+    	printf("n=%d\n", n);
     return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 
-int main(void)
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
