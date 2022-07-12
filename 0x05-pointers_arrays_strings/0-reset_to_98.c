4c792db9d3cc5aa22b2ab458988f828ad9b474c4
Ayandare Samuel <samuelayandare@gmail.com>
2022-07-12 10:15:05 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-//#include "main.h"
+#include "main.h"
 
 /**
  * reset_to_98 - reset integer pointed to by n to 98
@@ -6,25 +6,8 @@
  *
  * Return: void
  */
-/*void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 if (n)
 *n = 98;
-}*/
-#include "main.h"
-
-/** assign 98 to n
- * which was 402 before
- */
-
-int main(void)
-{
-    int n, *ptr_n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    ptr_n = &n;
-    *ptr_n = 'b';
-        printf("n=%d\n", n);
-    return 0;
 }
