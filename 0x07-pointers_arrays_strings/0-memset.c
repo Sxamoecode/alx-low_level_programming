7903939199a2192d1c87f0112c73fb97c046a25c
Ayandare Samuel <samuelayandare@gmail.com>
2022-07-18 05:33:47 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,13 @@
  * @n: number of spaces to fill
  * Return *s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		*(s + i) = b;
-	return (*s);
+
+	return (s);
 }
