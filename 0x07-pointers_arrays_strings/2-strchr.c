d6bb81dd5a2aac0be9453e9269613d290b6cbf8a
Ayandare Samuel <samuelayandare@gmail.com>
2022-07-18 06:22:50 -0700

    0

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * _strchr : string character
- * @s: string given
- * @c: another char
- *
- * Return: a string
- */
+* _strchr -> string character
+* @s: string given
+* @c: another char
+* Return: a string
+*/
 char *_strchr(char *s, char c)
 {
-	int a;
+		int a;
 
-	while (1)
-		a = *s++;
-		if (a == c)
-			return (s - 1);
-		if (a == 0)
-			return (NULL);
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }
