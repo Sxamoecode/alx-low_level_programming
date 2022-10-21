dc88b1cd27221d17e7eef3ab3f5af192b6746911
Ayandare Samuel <samuelayandare@gmail.com>
2022-10-21 10:19:01 -0700

    Updating

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,27 +8,25 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-    {
-         return NULL;
-    }
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
 
-    table->size = size;
-    table->array = malloc(size * sizeof(hash_node_t *));
-
-    if (table->array)
-    {
-        free(table);
-        return (NULL);
-    }
-    
-    for (i = 0; i < size; i++)
-    {
-        table->array[i] = NULL;
-    }
-    return (table);
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
